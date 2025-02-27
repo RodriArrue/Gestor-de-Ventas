2dba107064eefd3bb764c2b78fb57f952df20248
RodriArrue <arruerodrigo28@gmail.com>
2025-02-27 16:31:33 -0300

    segundo cambio

--- gestor_usuarios/views.py
+++ gestor_usuarios/views.py
@@ -3,20 +3,49 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import CustomUser, Vendedor, Cliente
 from .serializer import Userserializer, VendedorSerializer, ClienteSerializer 
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import action
+from django.contrib.auth import authenticate, login, logout
+from rest_framework.authtoken.models import Token
+from rest_framework.authentication import TokenAuthentication
 # Create your views here.
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = Userserializer
     permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
 
+    @action(detail=False, methods=['post'])
+    def login(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        user = authenticate(email=email, password=password)
+        if user:
+            token = Token.objects.create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+        return Response({'error': 'Credenciales Invalidas'}, status=status.HTTP_400_BAD_REQUEST)
 class VendedorViewSet(viewsets.ModelViewSet):
     queryset = Vendedor.objects.all()
     serializer_class = VendedorSerializer
     permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        serializer = VendedorSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
 class ClienteViewSet(viewsets.ModelViewSet):
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
     permission_classes = [IsAuthenticated]
 
+    def create(self, request, *args, **kwargs):
+        serializer = ClienteSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
